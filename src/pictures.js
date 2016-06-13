5ec206a8fbf4d7c45b656c4433e6af2cf79c31e7
lohmag <massalitine@mail.ru>
2016-06-14 01:01:07 +0300

    1

--- src/pictures.js
+++ src/pictures.js
@@ -2,7 +2,7 @@
  * Created by Nikita on 08.06.2016.
  */
 'use strict';
-var pictureContainer = document.querySelector('.pictures');
+
 var filters = document.querySelector('.filters');
 var templatePicture = document.querySelector('#picture-template');
 filters.className = 'filters hidden';
@@ -25,6 +25,41 @@ var getPictureClone = function(data, container) {
   container.appendChild(clone);
   return clone;
 };
+/*
 window.pictures.forEach(function(picture) {
   getPictureClone(picture, pictureContainer);
 });
+*/
+var getPictures = function(callback) {
+  var pictureContainer = document.querySelector('.pictures');
+  var pictures;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://o0.github.io/assets/json/pictures.json');
+  xhr.loadstart = function() {
+    pictureContainer.classList.add('pictures-loading');
+  };
+  xhr.onload = function(evt) {
+    pictureContainer.classList.remove('pictures-loading');
+    var requestObj = evt.target;
+    var response = requestObj.response;
+    pictures = JSON.parse(response);
+    callback(pictures);
+  };
+  xhr.error = function() {
+    pictureContainer.classList.remove('pictures-loading');
+    pictureContainer.classList.add('pictures-failure');
+  };
+  xhr.timeout = 10000;
+  xhr.ontimeout = function() {
+    pictureContainer.classList.remove('pictures-loading');
+    pictureContainer.classList.add('pictures-failure');
+  };
+  xhr.send();
+};
+var renderPictures = function(pictures) {
+  pictures.forEach(function(picture) {
+    var pictureContainer = document.querySelector('.pictures');
+    getPictureClone(picture, pictureContainer);
+  });
+};
+getPictures(renderPictures);
