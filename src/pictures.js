550fc9022ad2707be1712fbfcac196077e69441f
lohmag <massalitine@mail.ru>
2016-06-14 18:04:44 +0300

    2

--- src/pictures.js
+++ src/pictures.js
@@ -3,16 +3,18 @@
  */
 'use strict';
 
+var AJAX_SERVER_URL = 'https://o0.github.io/assets/json/pictures.json';
 var filters = document.querySelector('.filters');
-var templatePicture = document.querySelector('#picture-template');
-filters.className = 'filters hidden';
-var pictureClone;
-if ('content' in templatePicture) {
-  pictureClone = templatePicture.content.querySelector('.picture');
-} else {
-  pictureClone = templatePicture.querySelector('.picture');
-}
+filters.classList.add('hidden');
+
 var getPictureClone = function(data, container) {
+  var pictureClone;
+  var templatePicture = document.querySelector('#picture-template');
+  if ('content' in templatePicture) {
+    pictureClone = templatePicture.content.querySelector('.picture');
+  } else {
+    pictureClone = templatePicture.querySelector('.picture');
+  }
   var clone = pictureClone.cloneNode(true);
   clone.querySelector('.picture-comments').textContent = data.comments;
   clone.querySelector('.picture-likes').textContent = data.likes;
@@ -25,16 +27,12 @@ var getPictureClone = function(data, container) {
   container.appendChild(clone);
   return clone;
 };
-/*
-window.pictures.forEach(function(picture) {
-  getPictureClone(picture, pictureContainer);
-});
-*/
+
 var getPictures = function(callback) {
   var pictureContainer = document.querySelector('.pictures');
   var pictures;
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://o0.github.io/assets/json/pictures.json');
+  xhr.open('GET', AJAX_SERVER_URL);
   xhr.loadstart = function() {
     pictureContainer.classList.add('pictures-loading');
   };
@@ -43,6 +41,7 @@ var getPictures = function(callback) {
     var requestObj = evt.target;
     var response = requestObj.response;
     pictures = JSON.parse(response);
+    pictures = setFilters(filters, pictures);
     callback(pictures);
   };
   xhr.error = function() {
@@ -57,9 +56,55 @@ var getPictures = function(callback) {
   xhr.send();
 };
 var renderPictures = function(pictures) {
+  var pictureContainer = document.querySelector('.pictures');
   pictures.forEach(function(picture) {
-    var pictureContainer = document.querySelector('.pictures');
     getPictureClone(picture, pictureContainer);
   });
 };
+
+var setFilters = function(filters, pictures) {
+  var picturesDefault = pictures.slice(0);
+  filters.classList.remove('hidden');
+  var filtersRadio = document.getElementsByName('filter');
+  for (var i = 0; i < filtersRadio.length; i++) {
+    if (filtersRadio[i].type === 'radio' && filtersRadio[i].checked) {
+      switch (filtersRadio[i].id) {
+        case 'filter-popular':
+          return picturesDefault;
+          break;
+        case 'filter-new':
+          var picturesForLast4Days =  pictures.filter(function(picture) {
+            var FourDayBefore = Date.now() - 345600000;
+            var timestamp = new Date(picture.date);
+            var difference = timestamp.getTime() - FourDayBefore;
+            return difference >=0 && difference;
+          });
+          picturesForLast4Days.sort(function(a, b) {
+            var timestamp_1 = new Date(a.date);
+            var timestamp_2 = new Date(b.date);
+            return timestamp_2.getTime() - timestamp_1.getTime();
+          });
+          return picturesForLast4Days;
+        case 'filter-discussed':
+          pictures.sort(function(a, b) {
+            return b.comments - a.comments;
+          });
+          return pictures;
+          break;
+      }
+    }
+  }
+};
+
+var changeFilters = function() {
+  var filtersRadio = document.getElementsByName('filter');
+  for (var i = 0; i < filtersRadio.length; i++) {
+    filtersRadio[i].onchange = console.log(filtersRadio[i]);
+  }
+};
+
+var filtersRadio1 = document.getElementById('filter-new');
+filtersRadio1.onchange = console.log(filtersRadio1);
+changeFilters();
+
 getPictures(renderPictures);
